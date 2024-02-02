5a3c668113d8e5cd1235da9d2cdd243a1ca01e5e
Aditya <133390310+adityasanap24@users.noreply.github.com>
2024-02-02 21:43:59 +0530

    Add files via upload

---
+++ day_5_task.js
@@ -0,0 +1,64 @@
+let resume = {
+    besic: {
+      name: "aditya sanap",
+      DOB: "24/12/1998",
+      age: 26,
+      email: "adityasanap1122@gmail.com",
+      location: "nashik",
+    },
+    languages: [
+      {
+        languages: "marathi , english , hindi",
+      },
+    ],
+    education: [
+      {
+        "school name": "Purushottam English School - 10th - 72%",
+        " diploma":
+          "Electrical Diploma - 75% - K . V . N . Naik Diploma Engg Collage , Nashik, Maharashtra",
+        Degree:
+          "Computer Engg - appering - brahma valley college of engineering and research institute , Nashik , Maharashtra",
+        courses: "GUVI mern stack developer",
+      },
+    ],
+    profile: [
+      {
+        github: "https://github.com/adityasanap24",
+      },
+    ],
+  };
+  console.log(resume);
+
+  // let json = {
+  //   data: [
+  //     "name: aditya sanap",
+  //     "DOB: 24/12/1998",
+  //     "age: 26",
+  //     "email: adityasanap1122@gmail.com",
+  //     "location: nashik",
+  //   ],
+
+  //   object: {
+  //     name: "aditya sanap",
+  //     DOB: "24/12/1998",
+  //     age: "26",
+  //     email: "adityasanap1122@gmail.com",
+  //     location: "nashik",
+  //   },
+  // };
+
+  // console.log("forEach loop");
+  // json["data"].forEach((e) => console.log(e));
+
+  // console.log("for loop");
+  // for (let i = 0; i < json["data"].length; i++) {
+  //   console.log(json["data"][i]);
+  // }
+  // console.log("for of loop");
+  // for (const element of json["data"]) {
+  //   console.log(element);
+  // }
+  // console.log("for in loop");
+  // for (const element in json["object"]) {
+  //   console.log(json["object"][element]);
+  // }
